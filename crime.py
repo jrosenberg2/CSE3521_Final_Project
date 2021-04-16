11ebf005f5f05ac872c12b5f58a3e1d4f6b0469e
Jacob Rosenberg <jacobrosenberg242@gmail.com>
2021-04-16 01:13:08 -0400

    import and get data

--- crime.py
+++ crime.py
@@ -1,5 +1,14 @@
-import pandas as pd
-import math
-import numpy as np
-eps = np.finfo(float).eps
+import pandas as pd 
 
+#import the csv file
+df = pd.read_csv('FinalProject\Police_DepartmentIncidents-_PreviousYear__2016.csv')
+#drop all of the columns that we do not need
+df = df.drop(['IncidntNum', 'X', 'Y', 'Location', 'PdId'], axis=1)
+#Keep only the data where either someone was arrested or none
+df1 = df[df.Resolution == 'ARREST, BOOKED']
+df2 = df[df.Resolution == 'NONE']
+df = pd.concat([df1, df2])
+
+
+
+#df=df.sample(frac=1)
